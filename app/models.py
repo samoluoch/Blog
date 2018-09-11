9738129ed80031d871ecd1577e0413970caaa253
Sam Oluoch <samsoluoch@gmail.com>
2018-09-11 22:04:30 +0300

    imported UserMixin and defined the User class. Created the relationship table that relates the User, the Blog they create, and the Comments of the blog post

---
+++ app/models.py
@@ -0,0 +1,24 @@
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+from datetime import datetime
+from . import db
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+    '''
+    This is a User class that defines the object User and it's database tables
+    '''
+    
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255), index = True)
+    email = db.Column(db.String(255), unique = True, index = True)
+    password_hash = db.Column(db.String(255))
+    blog = db.relationship('Blog',backref = 'user',lazy = "dynamic")
+    user_id = db.relationship('Comment',backref = 'user',lazy = "dynamic")
+
