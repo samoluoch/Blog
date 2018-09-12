5130d7d9b32d0271aae4a53dc18221b9b11c2aa8
Sam Oluoch <samsoluoch@gmail.com>
2018-09-12 22:57:54 +0300

    created a User logout route that redirects the users to the logout page when they logout of the app

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,4 +1,7 @@
 @auth.route('/login',methods=['GET','POST'])
+'''
+This is a user login route that allows users to login
+'''
 def login():
     login_form = LoginForm()
     if login_form.validate_on_submit():
@@ -11,3 +14,17 @@ def login():
 
     title = "Blog login"
     return render_template('auth/login.html',login_form = login_form,title=title)
+
+
+@auth.route('/logout')
+'''
+This is a User logout route that redirects the users to the logout page after they logout
+'''
+@login_required
+def logout():
+    logout_user()
+    flash('You have successfully logged out')
+    return redirect(url_for("main.index"))
+
+
+
