0ff34bf8b6144b1c3cb2ccb1edb711db1aded152
Sam Oluoch <samsoluoch@gmail.com>
2018-09-12 23:01:22 +0300

    created user registration form that registers new users to the app

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -6,7 +6,25 @@ from ..models import User
 from wtforms import ValidationError
 
 class LoginForm(FlaskForm):
+    '''
+    This is a user login form that allows users to enter their login details and validates the details
+    '''
     email = StringField('Enter Your Email Address',validators=[Required(),Email()])
     password = PasswordField('Your Password',validators =[Required()])
     remember = BooleanField('Remember Password')
-    submit = SubmitField('Sign In')
+    submit = SubmitField('Login In')
+
+
+class RegistrationForm(FlaskForm):
+    '''
+    This is user registration form that allows new users to register on the app
+    '''
+    email = StringField('Your Email Address',validators=[Required(),Email()])
+    username = StringField('Enter your username',validators = [Required()])
+    password = PasswordField('Password',validators = [Required(),
+    EqualTo('password2',message = 'Passwords must match')])
+    password2 = PasswordField('Confirm Passwords',validators = [Required()])
+    submit = SubmitField('Sign Up')
+
+
+
