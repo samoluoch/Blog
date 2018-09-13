c7ba7e19a9588a907ecbc5798598b15ed6322d85
Sam Oluoch <samsoluoch@gmail.com>
2018-09-13 16:40:44 +0300

    created forms for capturing comment details when a user wants to comment on the post

---
+++ app/main/forms.py
@@ -0,0 +1,21 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField,SelectField
+from ..models import User
+from wtforms import ValidationError
+from wtforms.validators import Required, Email, EqualTo
+
+class PostForm(FlaskForm): #create a class that inherits from FlaskForm class
+    category = SelectField('Category', choices =[('General','General'),('Cars','Cars'),('Technology','Technology')],validators=[Required()])
+    post = TextAreaField('Post', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+class CommentsForm(FlaskForm):
+    comments = TextAreaField('Comment on the Post', validators=[Required()])
+    submit = SubmitField('Submit')
+
+class ContentForm(FlaskForm):
+    content = TextAreaField('YOUR POST')
+    submit = SubmitField('SUBMIT')
+
+
--- app/main/views.py
+++ app/main/views.py
@@ -1,19 +1,22 @@
 from flask import render_template, request, redirect, url_for, abort, flash
 from . import main
 # from .. import db
-# from ..models import User
+from ..models import User
 from app import login_manager
+from .forms import PostForm
 @main.route('/',methods=['GET','POST'])
 def index():
     '''
     View page function that returns the pitch titles on the index page
     '''
-    # form = PostForm()
+    form = PostForm()
 
-    # if form.validate_on_submit():
-    #     new_post = Blog(actual_post=form.post.data,category=form.category.data, user_id=current_user.id)
-    #     new_post.save_post()
-    #     flash('Post has been created successfully')
-    # Post = Post.query.filter_by(id)
+    if form.validate_on_submit():
+        new_post = Post(actual_post=form.post.data,category=form.category.data, user_id=current_user.id)
+        new_post.save_post()
+        flash('Post has been created successfully')
+    Post = Post.query.filter_by(id)
 
     return render_template('index.html',title = 'new_post')
+
+
